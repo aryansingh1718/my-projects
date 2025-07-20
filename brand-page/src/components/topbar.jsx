ac5a9a6cfefa0b26bc226fb061c5aec34c7463cd
aryan singh <aryansinghpm27@gmail.com>
2025-07-20 20:57:59 +0530

    Update brand-page: [brief description of what changed]

--- brand-page/src/components/topbar.jsx
+++ brand-page/src/components/topbar.jsx
@@ -1,16 +1,16 @@
 export default function Navbar(){
     return <div>
-         <nav className="grid grid-cols-10 h-18 ">
-        <div className="col-span-4">
+         <nav className="grid grid-cols-1 sm:grid-cols-10">
+        <div className="col-span-1 md:col-span-4">
             <img src="https://tse1.mm.bing.net/th/id/OIP.OFAJjiEH5qsHo77r9dY64gHaEK?pid=Api&rs=1&c=1&qlt=95&w=219&h=123" className="w-30 h-20 p-3 ml-3"></img>
         </div>
-        <div className="col-span-4 flex flex-row gap-7 p-4 font-semibold my-1">
+        <div className="col-span-1 flex flex-row gap-7 p-4 font-semibold my-1 sm:col-span-4">
             <button className="cursor-pointer">MENU</button>
             <button className="cursor-pointer">LOCATION</button>
             <button className="cursor-pointer">ABOUT</button>
             <button className="cursor-pointer">CONTACT</button>
         </div>
-        <div className="col-span-2">
+        <div className="col-span-1 sm:col-span-2">
             <button className="bg-red-600 ml-25 mt-7 pl-2 pr-2 pt-1 pb-1 rounded-md cursor-pointer text-white">Login</button>
         </div>
       </nav>
